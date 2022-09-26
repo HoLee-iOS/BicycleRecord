f7084136d5d85431cd5648626549b830d399830e
skyler <ho971122@gmail.com>
2022-09-27 01:57:45 +0900

    Initial Commit

---
+++ BicycleRecord/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BicycleRecord
+//
+//  Created by 이현호 on 2022/09/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
